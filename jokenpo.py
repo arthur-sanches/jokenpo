9ac7d09801e334e6dd0f5256b0ee454c01f9f53d
arthur-sanches <arthur.sanches96@gmail.com>
2022-05-18 21:05:40 -0300

    Code refactor, from procedural to object oriented programming

--- jokenpo.py
+++ jokenpo.py
@@ -1,88 +1,95 @@
 import sys
 from random import randint
 
-game_active = True
-weapons = ("Rock", "Paper", "Scissors")
+
+class Jokenpo:
+
+    def __init__(self):
+        self.game_active = True
+        self.weapons = ("Rock", "Paper", "Scissors")
+
+    def run_game(self):
+        while self.game_active:
+            print("Press 1 to play indefinitely.")
+            print("Press 2 to play best of 3.")
+            print("Press 3 to play best of 5.")
+            print("Press Q to quit at any time.")
+
+            choice = input()
+            self._check_quit(choice)
+
+            self._reset_game()
+
+            if choice == "1":
+                self.infinite_game()
+            elif choice == "2":
+                self.best_three_game()
+            elif choice == "3":
+                self.best_five_game()
+
+    def infinite_game(self):
+        while True:
+            self._single_game()
+
+    def best_three_game(self):
+        self._limited_game(3)
+
+    def best_five_game(self):
+        self._limited_game(5)
+
+    def _single_game(self):
+        user_key_press = input(
+            "Choose your weapon: (1)Rock (2)Paper (3)Scissors: ")
+        self._check_quit(user_key_press)
+
+        try:
+            user_choice = int(user_key_press) - 1
+            computer_choice = randint(0, 2)
+            result = user_choice - computer_choice
+
+            if (result == 0):
+                self._chosen_weapons(user_choice, computer_choice)
+                print("\nIt's a draw!\n")
+            elif (result == 1) or (result == -2):
+                self._chosen_weapons(user_choice, computer_choice)
+                print("\nYou won!\n")
+                self.user_score += 1
+            elif (result == -1) or (result == 2):
+                self._chosen_weapons(user_choice, computer_choice)
+                print("\nThe computer won :(\n")
+                self.computer_score += 1
+        except:
+            print(f"'{user_key_press}' is not a valid choice.")
+        print(
+            f"The present score is User: [{self.user_score}] and Computer: [{self.computer_score}]")
+
+    def _limited_game(self, max_matches):
+        wins_needed = (max_matches//2) + 1
+        while (self.user_score < wins_needed and self.computer_score < wins_needed):
+            self._single_game()
+        self._game_result_msg(wins_needed)
+
+    def _game_result_msg(self, wins_needed):
+        if self.user_score == wins_needed:
+            print("\n Congratulations you won! \n")
+        else:
+            print("\n Sorry but the computer won :(\n")
+
+    def _chosen_weapons(self, user_choice, computer_choice):
+        print(f"\n         You chose: {self.weapons[user_choice]}")
+        print(f"The computer chose: {self.weapons[computer_choice]}")
+
+    def _reset_game(self):
+        self.user_score = 0
+        self.computer_score = 0
+
+    def _check_quit(self, key_press):
+        if type(key_press) == str:
+            if key_press.capitalize() == "Q":
+                sys.exit()
 
 
-def single_game():
-    global user_score, computer_score
-    user_key_press = input(
-        "Choose your weapon: (1)Rock (2)Paper (3)Scissors: ")
-    check_quit(user_key_press)
-
-    user_choice = int(user_key_press) - 1
-    computer_choice = randint(0, 2)
-    result = user_choice - computer_choice
-
-    if (result == 0):
-        chosen_weapons(user_choice, computer_choice)
-        print("\nIt's a draw!\n")
-    elif (result == 1) or (result == -2):
-        chosen_weapons(user_choice, computer_choice)
-        print("\nYou won!\n")
-        user_score += 1
-    elif (result == -1) or (result == 2):
-        chosen_weapons(user_choice, computer_choice)
-        print("\nThe computer won :(\n")
-        computer_score += 1
-    print(
-        f"The present score is User: {user_score}! and Computer: {computer_score}!")
-
-
-def limited_game(max_matches):
-    wins_needed = (max_matches//2) + 1
-    while (user_score < wins_needed and computer_score < wins_needed):
-        single_game()
-    game_result_msg(wins_needed)
-
-
-def infinite_game():
-    while True:
-        single_game()
-
-
-def best_three_game():
-    limited_game(3)
-
-
-def best_five_game():
-    limited_game(5)
-
-
-def game_result_msg(wins_needed):
-    if user_score == wins_needed:
-        print("\n Congratulations you won! \n")
-    else:
-        print("\n Sorry but the computer won :(\n")
-
-
-def chosen_weapons(user_choice, computer_choice):
-    print(f"\n         You chose: {weapons[user_choice]}")
-    print(f"The computer chose: {weapons[computer_choice]}")
-
-
-def check_quit(key_press):
-    if type(key_press) == str:
-        if key_press.capitalize() == "Q":
-            sys.exit()
-
-
-while game_active:
-    print("Press 1 to play indefinitely.")
-    print("Press 2 to play best of 3.")
-    print("Press 3 to play best of 5.")
-    print("Press Q to quit at any time.")
-
-    choice = input()
-    check_quit(choice)
-
-    user_score = 0
-    computer_score = 0
-
-    if choice == "1":
-        infinite_game()
-    elif choice == "2":
-        best_three_game()
-    elif choice == "3":
-        best_five_game()
+if __name__ == '__main__':
+    # Make a game instance and run the game.
+    jk = Jokenpo()
+    jk.run_game()
