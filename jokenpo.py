9bd0fac2a1bc32041ef53471095f71bf4058d931
arthur-sanches <arthur.sanches96@gmail.com>
2022-06-12 22:27:04 -0300

    Add reset button and remove unused functions

--- jokenpo.py
+++ jokenpo.py
@@ -16,39 +16,11 @@ class Jokenpo(QDialog):
         self.computer_score = 0
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.rock_button.clicked.connect(self.rock_chosen)
-        self.ui.paper_button.clicked.connect(self.paper_chosen)
-        self.ui.scissors_button.clicked.connect(self.scissors_chosen)
+        self.ui.button_rock.clicked.connect(self.rock_chosen)
+        self.ui.button_paper.clicked.connect(self.paper_chosen)
+        self.ui.button_scissors.clicked.connect(self.scissors_chosen)
+        self.ui.button_reset.clicked.connect(self._reset_game)
         self.show()
-
-    def run_game(self):
-        while self.game_active:
-            print("Press 1 to play indefinitely.")
-            print("Press 2 to play best of 3.")
-            print("Press 3 to play best of 5.")
-            print("Press Q to quit at any time.")
-
-            choice = input()
-            self._check_quit(choice)
-
-            self._reset_game()
-
-            if choice == "1":
-                self.infinite_game()
-            elif choice == "2":
-                self.best_three_game()
-            elif choice == "3":
-                self.best_five_game()
-
-    def infinite_game(self):
-        while True:
-            self._single_game()
-
-    def best_three_game(self):
-        self._limited_game(3)
-
-    def best_five_game(self):
-        self._limited_game(5)
 
     def rock_chosen(self):
         self._single_game(0)
@@ -60,9 +32,6 @@ class Jokenpo(QDialog):
         self._single_game(2)
 
     def _single_game(self, choice):
-        # user_key_press = input(
-        #     "Choose your weapon: (1)Rock (2)Paper (3)Scissors: ")
-        # self._check_quit(user_key_press)
         user_key_press = choice
 
         try:
@@ -89,35 +58,22 @@ class Jokenpo(QDialog):
         except:
             print(f"'{user_key_press}' is not a valid choice.")
         finally:
-            self.ui.label_your_score.setText(str(self.user_score))
-            self.ui.label_foe_score.setText(str(self.computer_score))
-            print(
-                f"The present score is User: [{self.user_score}] and Computer: [{self.computer_score}]")
-
-    def _limited_game(self, max_matches):
-        wins_needed = (max_matches//2) + 1
-        while (self.user_score < wins_needed and self.computer_score < wins_needed):
-            self._single_game()
-        self._game_result_msg(wins_needed)
-
-    def _game_result_msg(self, wins_needed):
-        if self.user_score == wins_needed:
-            print("\n Congratulations you won! \n")
-        else:
-            print("\n Sorry but the computer won :(\n")
+            self._update_score()
 
     def _chosen_weapons(self, user_choice, computer_choice):
         print(f"\n         You chose: {self.weapons[user_choice]}")
         print(f"The computer chose: {self.weapons[computer_choice]}")
 
+    def _update_score(self):
+        self.ui.label_your_score.setText(str(self.user_score))
+        self.ui.label_foe_score.setText(str(self.computer_score))
+        print(
+            f"The present score is User: [{self.user_score}] and Computer: [{self.computer_score}]")
+
     def _reset_game(self):
         self.user_score = 0
         self.computer_score = 0
-
-    def _check_quit(self, key_press):
-        if type(key_press) == str:
-            if key_press.capitalize() == "Q":
-                sys.exit()
+        self._update_score()
 
 
 if __name__ == '__main__':
